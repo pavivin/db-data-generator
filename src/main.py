45af4537b94a84a626bdaacf1ad8cda31f5f0d43
pavivin <pavivin@yandex.ru>
2022-08-07 08:54:45 +0300

    опциональные параметры из json

--- src/main.py
+++ src/main.py
@@ -8,55 +8,30 @@ from utils.base import pascal_to_snake, read_json_file, snake_to_pascal, str_to_
 from utils.constants import OutputTypes
 from utils.models import TableBase, TableColumn
 
-
-# def _generate_columns(class_item: type):
-#     columns = []
-
-# columns.append(table_column)
-
-# return columns
-
-# for column_name, column in table.items():
-#     if column_name != 'rows_count':
-#         column_class = HINTING_TO_CLASSES[table_class.__annotations__[column_name].__name__]
-#         values_select = column['values_select']
-#         table_column = TableColumn(
-#             column_name=column_name,
-#             column_config=column_class(
-#                 values_select=[
-#                     base_types.ValuesFrequency(
-#                         value=item['values_frequency']['value'],
-#                         frequency=item['values_frequency'].get('frequency'),
-#                     )
-#                     for item in values_select
-#                 ]
-#             ),
-#         )
-
-
 def _get_json_table_data(table_name: str):
     return config['tables'].get(table_name)
 
 
-def _generate_columns(class_name: str, table_class: type):
+def _generate_columns(table_name: str, table_class: type):
     columns = []
     json_table_data = _get_json_table_data(table_name)
     for column_name, column_type in table_class.__annotations__.items():
-        values_select = json_table_data[column_name]['values_select']
-        columns.append(
-            TableColumn(
-                column_name=column_name,
-                column_config=HINTING_TO_CLASSES[column_type.__name__](
-                    values_select=[
-                        base_types.ValuesFrequency(
-                            value=item['values_frequency']['value'],
-                            frequency=item['values_frequency'].get('frequency'),
-                        )
-                        for item in values_select
-                    ]
-                ),
+        column_data = json_table_data.get(column_name)
+        if column_data:
+            columns.append(
+                TableColumn(
+                    column_name=column_name,
+                    column_config=HINTING_TO_CLASSES[column_type.__name__](
+                        values_select=[
+                            base_types.ValuesFrequency(
+                                value=item['values_frequency'].get('value'),
+                                frequency=item['values_frequency'].get('frequency'),
+                            )
+                            for item in column_data['values_select']
+                        ]
+                    ),
+                )
             )
-        )
     return columns
 
 
@@ -81,39 +56,24 @@ for class_name in all_dataclasses:
     except AttributeError as e:
         raise RuntimeError(f'Таблица {e.name} не найдена')
 
+    rows_count = config['tables'].get(table_name, {'rows_count': 100}).get('rows_count')
+
     tables.append(
         TableBase(
             table_name=class_name,
-            rows_to_generate=config['tables'][table_name].get('rows_count'),
+            rows_to_generate=rows_count,
             output_format=OutputTypes.CSV,
             columns=_generate_columns(class_name, table_class),
         )
     )
 
-# tables = []
-# for table_name, table in config['tables'].items():
-#     class_name = snake_to_pascal(table_name)
-#     try:
-#         table_class = str_to_class(class_name)
-#     except AttributeError as e:
-#         raise RuntimeError(f'Таблица {e.name} не найдена')
-
-#     tables.append(
-#         TableBase(
-#             table_name=table_name,
-#             rows_to_generate=table['rows_count'],
-#             output_format=OutputTypes.CSV,
-#             columns=_generate_columns(table, table_name),
-#         )
-#     )
-
 
 generator = data_generator.TableDataGeneratorService(tables)
-# start_date = datetime.now()
+start_date = datetime.now()
 
 generator.generate_table_data()
 
-# print(datetime.now() - start_date)
+print(datetime.now() - start_date)
 
-# print('INFO: press Enter to continue')
-# input()
+print('INFO: press Enter to continue')
+input()
